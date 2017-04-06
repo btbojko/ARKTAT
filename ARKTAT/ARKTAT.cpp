87578dc172bfc9a5250eb071b4f2c08ea675ef14
bbojko <bbojko@first-rule.wd.navair-rdte.navy.mil>
2017-04-05 17:39:26 -0700

    Testing data structures

--- ARKTAT/ARKTAT.cpp
+++ ARKTAT/ARKTAT.cpp
@@ -5,11 +5,24 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C, Ansi-style
 //============================================================================
-
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <src/material/test_function.cpp>
+
+using namespace std;
+
 int main(void) {
-	puts("Hello World!!!");
-	return EXIT_SUCCESS;
+	Box box1;
+	box1.setLength(2.);
+    box1.setHeight(3.);
+    box1.setDepth(4.);
+
+    double volume = 0.;
+
+	cout << "Hello World!" << endl;
+
+	volume = box1.getVolume();
+	cout << "I created a box with a volume of : " << volume << endl;
 }
