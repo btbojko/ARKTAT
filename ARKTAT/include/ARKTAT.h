e75396af91803f7280db189c8e1eaa3522532c2f
bbojko <bbojko@first-rule.wd.navair-rdte.navy.mil>
2017-04-06 16:44:24 -0700

    adding in new files

---
+++ ARKTAT/include/ARKTAT.h
@@ -0,0 +1,16 @@
+/*
+ * ARKTAT.h
+ *
+ *  Created on: Apr 6, 2017
+ *      Author: bbojko
+ */
+
+#ifndef ARKTAT_H_
+#define ARKTAT_H_
+
+#include <include/material/test_function.h>
+//#include <src/material/test_function.cpp>
+
+namespace ARKTAT{}
+
+#endif /* ARKTAT_H_ */
